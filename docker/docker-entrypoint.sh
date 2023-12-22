b1992f8803435d39769833fdaa2bde95847d61ec
adr1an <adrn.cederberg123@gmail.com>
2023-12-22 10:21:57 -0700

    Updated docker-entrypoint.sh to include echoed details, (selfishly) added jedi-language-server to dev dependencies.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,11 +1,36 @@
 # NOTE: This will be mounted as the bashrc for the default user.
+
+# Go to WORKDIR
 cd /app
-source venv/bin/activate
 
-EXISTS=$( pip list | grep documents-server )
-if [[ ! $EXISTS ]]; then python -m pip install --editable .; fi
 
-EXISTS=$( pip list | grep pytest )
-if [[ ! $EXISTS ]]; then python -m pip install .[test]; fi
+# Virtual environment
+if [[ ! -d .venv ]]; then
+  echo "Virtual environment does not exist. Creating under \`.venv\`."
+  python -m venv .venv
+else
+  echo "Virtual environment already exists.";
+fi
+
+source .venv/bin/activate
+
+
+# Install app and test dependencies.
+EXISTS=$( pip list 2> /dev/null | grep articles-api )
+if [[ ! $EXISTS ]]; then
+  echo "Python project not installed in virtual environment. Installing."
+  python -m pip install --editable .
+else
+  echo "Python project already installed in virtual environment."
+fi
+
+
+EXISTS=$( pip list 2> /dev/null | grep pytest )
+if [[ ! $EXISTS ]]; then
+  echo "Python project test dependencies not installed. Installing."
+  python -m pip install .[test];
+else
+  echo "Python project test dependencies already installed."
+fi
 
 export PATH="$PATH:$(realpath ~/.local/bin)"
